523e5742c39d73f3dc31d2df3f2d4823c2e848be
Michelle Andrews <insomniacdeziner@gmail.com>
2016-02-08 15:28:52 -0500

    MA: misc updates to blog staying

--- partials/footer-about.php
+++ partials/footer-about.php
@@ -1,5 +1,5 @@
 <section class="footer--about">
-	<div class="footer__title">About Rethink Solutions</div>
+	<div class="footer__title">Rethink Solutions</div>
 	<p>Rethink Solutions Inc. is an industry leading developer and vendor of web based, real estate asset management solutions in the areas of property tax and assessment, occupancy cost, and insurance.</p>
 		
 </section>
