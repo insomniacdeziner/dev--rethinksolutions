cf8e583fc7db983c6f08cac6d55bfa1ad85664ff
Michelle Andrews <insomniacdeziner@gmail.com>
2016-02-10 10:46:01 -0500

    MA: updated services page with mobile fixes

--- page-services.php
+++ page-services.php
@@ -6,12 +6,15 @@
 <?php get_header(); ?>
 
 <main class="main" role="main">
+
 <?php get_template_part('partials/section', 'banner'); ?> 
 <div class="wrap">
 	<?php get_template_part('partials/section', 'page-title'); ?> 
 	<?php get_template_part('partials/nav'); ?>
 </div> 
-<?php get_template_part('partials/section-tabs'); ?> 
+<div class="services">
+	<?php get_template_part('partials/section-tabs'); ?> 
+</div>
 <?php get_template_part('partials/section', 'clients'); ?> 	
 <?php get_template_part('partials/section', 'request-demo'); ?> 	
 
