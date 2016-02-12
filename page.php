3cf8b915dd1a9089f3766c0be2d022f4cc165c91
Michelle Andrews <insomniacdeziner@gmail.com>
2016-02-12 10:22:42 -0500

    MA: refactored buttons, fixed sidebar and left content area for responsive

--- page.php
+++ page.php
@@ -1,10 +1,9 @@
 <?php get_header(); ?>
 
-			<div id="content">
+		
+				<div class="wrap">
 
-				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<main id="main" class="page--sidebar" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -62,6 +61,5 @@
 
 				</div>
 
-			</div>
 
 <?php get_footer(); ?>
